78bb3705172bcad9a1d80193e6bdfb675eb97904
Aaron <136681119+Mr-Tosty@users.noreply.github.com>
2024-09-04 12:13:25 -0600

    Confirmar

---
+++ Calculadora avanzada/src/Calculos/Basicos/Basico.java
@@ -0,0 +1,191 @@
+package Calculos.Basicos;
+public class Basico {
+    // Clase base para la suma de variables
+    public abstract class Suma {
+        protected int VS1;
+        protected int VS2;
+        protected int VS3;
+        protected int VS4;
+        protected int VS5;
+        protected int VS6;
+        public Suma(int VS1, int VS2, int VS3, int VS4, int VS5, int VS6) {
+            this.VS1 = VS1;
+            this.VS2 = VS2;
+            this.VS3 = VS3;
+            this.VS4 = VS4;
+            this.VS5 = VS5;
+            this.VS6 = VS6;
+        }
+        public abstract int sumar(); // Metodo abstracto para la suma
+    }
+    // Clase para sumar 6 variables
+    public class RSV6 extends Suma {
+        public RSV6(int VS1, int VS2, int VS3, int VS4, int VS5, int VS6) {
+            super(VS1, VS2, VS3, VS4, VS5, VS6);
+        }
+        @Override
+        public int sumar() {
+            System.out.println("Suma de 6 variables");
+            return VS1 + VS2 + VS3 + VS4 + VS5 + VS6;
+        }
+    }
+    // Clase para sumar 5 variables
+    public class RSV5 extends Suma {
+        public RSV5(int VS1, int VS2, int VS3, int VS4, int VS5) {
+            super(VS1, VS2, VS3, VS4, VS5, 0); 
+        }
+        @Override
+        public int sumar() {
+            System.out.println("Suma de 5 variables");
+            return VS1 + VS2 + VS3 + VS4 + VS5;
+        }
+    }
+    // Clase para sumar 4 variables
+    public class RSV4 extends Suma {
+        public RSV4(int VS1, int VS2, int VS3, int VS4) {
+            super(VS1, VS2, VS3, VS4, 0, 0); 
+        }
+        @Override
+        public int sumar() {
+            return VS1 + VS2 + VS3 + VS4;
+        }
+    }
+    // Clase para sumar 3 variables
+    public class RSV3 extends Suma {
+        public RSV3(int VS1, int VS2, int VS3) {
+            super(VS1, VS2, VS3, 0, 0, 0);
+        }
+        @Override
+        public int sumar() {
+            return VS1 + VS2 + VS3;
+        }
+    }
+    // Clase para sumar 2 variables
+    public class RSV2 extends Suma {
+        public RSV2(int VS1, int VS2) {
+            super(VS1, VS2, 0, 0, 0, 0);
+        }
+        @Override
+        public int sumar() {
+            return VS1 + VS2;
+        }
+    }
+    //Clase para la base resta
+    public abstract class Resta{
+        protected int VR1;
+        protected int VR2;
+        protected int VR3;
+        protected int VR4;
+        protected int VR5;
+        protected int VR6;
+        public Resta (int VR1, int VR2, int VR3, int VR4, int VR5, int VR6){
+            this.VR1 = VR1;
+            this.VR2 = VR2;
+            this.VR3 = VR3;
+            this.VR4 = VR4;
+            this.VR5 = VR5;
+            this.VR6 = VR6;
+        }
+        public abstract int restar(); //Metodo abstracto de restar
+    }
+    //Clase para restar 6 variables
+    public class RRV6 extends Resta{
+        public RRV6(int VR1, int VR2, int VR3, int VR4, int VR5, int VR6){
+            super(VR1, VR2, VR3, VR4, VR5, VR6);
+        }
+        @Override
+        public int restar(){
+            return VR1 - VR2 - VR3 - VR4 - VR5 - VR6;
+        }
+    }
+    //Clase para restar 5 variables
+    public class RRV5 extends Resta{
+        public RRV5(int VR1, int VR2, int VR3, int VR4, int VR5){
+            super(VR1, VR2, VR3, VR4, VR5, 0);
+        }
+        @Override
+        public int restar(){
+            return VR1 - VR2 - VR3 - VR3 - VR5;
+        }
+    }
+    //Clase para restar 4 variables
+    public class RRV4 extends Resta{
+        public RRV4 (int VR1, int VR2, int VR3, int VR4){
+            super(VR1, VR2, VR3, VR4, 0, 0);
+        }
+        @Override
+        public int restar(){
+            return VR1 - VR2 - VR3 - VR4;
+        }
+    }
+    //Clase para restar 3 variables
+    public class RRV3 extends Resta{
+        public RRV3 (int VR1, int VR2, int VR3){
+            super(VR1, VR2, VR3, 0, 0, 0);
+        }
+        @Override
+        public int restar(){
+            return VR1 - VR2 - VR3;
+        }
+    }
+    //Clase para restar 2 variables
+    public class RRV2 extends Resta{
+        public RRV2 (int VR1, int VR2){
+            super(VR1, VR2, 0, 0, 0, 0);
+        }
+        @Override
+        public int restar(){
+            return VR1 - VR2;
+        }
+    }
+    //Clase para la base multiplicacion
+    public abstract class Multiplicacion{
+        protected int VM1;
+        protected int VM2;
+        protected int VM3;
+        protected int VM4;
+        protected int VM5;
+        protected int VM6;
+        public Multiplicacion(int VM1, int VM2, int VM3, int VM4, int VM5, int VM6){
+            this.VM1 = VM1;
+            this.VM2 = VM2;
+            this.VM3 = VM3;
+            this.VM4 = VM4;
+            this.VM5 = VM5;
+            this.VM6 = VM6;
+        }
+        public abstract int multiplicar();//Metodo abstracto de multiplicar
+    }
+    //Clase para multiplicar 6 variables
+    public class RMV6 extends Multiplicacion{
+        public RMV6 (int VM1, int VM2, int VM3, int VM4, int VM5, int VM6){
+            super(VM1, VM2, VM3, VM4, VM5, VM6);
+        }
+        @Override
+        public int multiplicar(){
+            return VM1 * VM2 * VM3 * VM4 * VM5* VM6;
+        }
+    }
+    //Clase para multiplicar 5 variables
+    public class RMV5 extends Multiplicacion{
+        public RMV5 (int VM1, int VM2, int VM3, int VM4, int VM5){
+            super(VM1, VM2, VM3, VM4, VM5, 0);
+        }
+        @Override
+        public int multiplicar(){
+            return VM1 * VM2 * VM3 * VM4 * VM5;
+        }
+    }
+    //Clase para multiplicar 4 variables
+    public class RMV4 extends Multiplicacion{
+        public RMV4 (int VM1, int VM2, int VM3, int VM4){
+            super(VM1, VM2, VM3, VM4, 0, 0);
+        }
+        @Override
+        public int multiplicar(){
+            return VM1 * VM2 * VM3 * VM4;
+        }
+    }
+    //Clase para multiplicar 3 variable
+    
+}
