56132f6c749c14190d8270f90cf2d7d8581e557b
Aaron <136681119+Mr-Tosty@users.noreply.github.com>
2024-09-12 09:10:59 -0600

    Confirmar

--- Calculadora avanzada/src/Basico/BasicoOperaciones.java
+++ Calculadora avanzada/src/Basico/BasicoOperaciones.java
@@ -454,9 +454,63 @@ public class BasicoOperaciones {
         }
     }
     //Clase para 5 variables aleatorio
+    public class VAs5 extends Aleatorio{
+        public VAs5(){
+            super(0, 0, 0, 0, 0, 0);
+        }
+        @Override
+        public int[] alearotizar(){
+            VA1 = (int) Math.random()*1000;
+            VA2 = (int) Math.random()*1000;
+            VA3 = (int) Math.random()*1000;
+            VA4 = (int) Math.random()*1000;
+            VA5 = (int) Math.random()*1000;
+            System.out.println("Aleatorio de 5 variable");
+            return new int[]{VA1, VA2, VA3, VA4, VA5};
+        }
+    }
     //Clase para 4 variables aleatorio
+    public class VAs4 extends Aleatorio{
+        public VAs4(){
+            super(0, 0, 0, 0, 0, 0);
+        }
+        @Override
+        public int[] alearotizar(){
+            VA1 = (int) Math.random()*1000;
+            VA2 = (int) Math.random()*1000;
+            VA3 = (int) Math.random()*1000;
+            VA4 = (int) Math.random()*1000;
+            System.out.println("Aleatorio de 4 variable");
+            return new int[]{VA1, VA2, VA3, VA4};
+        }
+    }
     //Clase para 3 variables aleatorio
+    public class VAs3 extends Aleatorio{
+        public VAs3(){
+            super(0, 0, 0, 0, 0, 0);
+        }
+        @Override
+        public int[] alearotizar(){
+            VA1 = (int) Math.random()*1000;
+            VA2 = (int) Math.random()*1000;
+            VA3 = (int) Math.random()*1000;
+            System.out.println("Aleatorio de 3 variable");
+            return new int[]{VA1, VA2, VA3};
+        }
+    }
     //Clase para 2 variables aleatorio
+    public class VAs2 extends Aleatorio{
+        public VAs2(){
+            super(0, 0, 0, 0, 0, 0);
+        }
+        @Override
+        public int[] alearotizar(){
+            VA1 = (int) Math.random()*1000;
+            VA2 = (int) Math.random()*1000;
+            System.out.println("Aleatorio de 2 variable");
+            return new int[]{VA1, VA2};
+        }
+    }
     //Clase para 1 variables aleatorio
     public class VAs1 extends Aleatorio{
         public VAs1(int VA1){
