8a0b109019b9a2fec738a8c772542b6140df1014
Aaron <136681119+Mr-Tosty@users.noreply.github.com>
2024-09-18 10:14:21 -0600

    Primer integracion de ter dimenciones

--- Calculadora avanzada/src/Cubica/CubicaOperaciones.java
+++ Calculadora avanzada/src/Cubica/CubicaOperaciones.java
@@ -1,4 +1,11 @@
 package Cubica;
 public class CubicaOperaciones{
-    
+    public abstract class Cubo{
+        //Variable Lado Cuadrado A
+        protected int VLCA;
+        public Cuadrado(int LCA){
+            this.VLCA = VLCA;
+        }
+        public abstract int cubo();//Metodo abstracto cuadrado
+    }
 }
