7d30c15a16766812c94bd5aad48da781f0815d70
Aaron <136681119+Mr-Tosty@users.noreply.github.com>
2024-11-18 11:01:33 -0600

    Agregar la base de la tabla de multiplicacion

--- Calculadora avanzada/src/Aritmetica/AritmeticaOperaciones.java
+++ Calculadora avanzada/src/Aritmetica/AritmeticaOperaciones.java
@@ -225,4 +225,55 @@ public class AritmeticaOperaciones{
             Sytem.out.println("Division de fraccion de 2 fracciones y 4 variables");
         }
     }
+    //Clase para la tabla de multiplicar
+    public abstract class TablaMultiplicar{
+        //Variable Tabla de Multiplicar base
+        protected int VTMB;
+        //Variable Tabla Multiplicar Limite
+        protected int VTML;
+        //Resultado Tabla Multiplicar Completo Operaciones
+        protected int RTMCO;
+        //Resultado Tabla Multiplicar Operaciones
+        protected int RTMO;
+        public TablaMultiplicar{
+        this.VTMB;
+        }
+        public abstract int[]tablamultiplicarcompleto(); //Metodo abstracto de tabla multiplicar completo
+        public abstract int[]tablamultiplicar();//Metodo abstracto de tabla de multiplicar
+    }
+    //Clase para la tabla de multiplicar completo
+    public class RTMC extends TablaMultiplicar{
+        public RMTC (int VTMB, int VTML){
+            super(VTMB, 0);
+        }
+        @Override
+        public int[] tablamultiplicarcompleto(){
+            System.out.println("Tabla de multiplicar completo");
+            //Tabla Multiplicar Completo Repetir
+            for (int TMCR = 1; TMCR <= 10; TMCR++){
+                int RTMCO = TMCR * VTMB;
+            }
+            return new int[] {RTMCO, TMCR, VTMB};
+        }
+    }
+    public class RTM extends TablaMultiplicar{
+        public RTM (int VTMB, int VTML){
+            super(VTMB, VTML);
+        }
+        @Override
+        public int[] tablamultiplicar(){
+            System.out.println("Tabla de multiplicar limite");
+            if(TMR = VTML){
+                //Tabla Multiplicar Repetir
+                for(int TMR = 1; TMR <= VTML; TMR++){
+                    int RTMO = TMR * VTMB;
+                }
+                return new int[] {RTMO, TMR, VTMB};
+            }else{
+                Sytem.out.println("Es invalido el multiplicador");
+                throw new ArithmeticException("No se puede dividir entre 0");
+                return - 1;
+            }
+        }
+    }
 }
