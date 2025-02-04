ce3406b9798797f667b116d7fb5a03d2b3c46a0a
Aaron <soldadofuturo009@gmail.com>
2025-02-04 12:34:25 -0600

    Modificacion del paquete trigonometria

--- Calculadora avanzada/src/Trigonometria/Seno.java
+++ Calculadora avanzada/src/Trigonometria/Seno.java
@@ -1,12 +1,10 @@
 package Trigonometria;
-public class TrigonometriaOperaciones{
-    //Clase base de seno
-    public abstract class Seno{
-        //Variable Seno
-        protected int VS;
-        public Seno(int VS){
-            this.VS = VS;
-        }
-        public abstract int seno(); //Metodo abstracto seno
+//Clase base de seno
+public abstract class Seno{
+    //Variable Seno
+    protected int VS;
+    public Seno(int VS){
+        this.VS = VS;
     }
+    public abstract int seno(); //Metodo abstracto seno
 }
